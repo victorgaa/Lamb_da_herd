84b91b9b791a2d04731a25d0b8ae87ed5d6d5089
ICS-Victor <ICS25-036@camosun.ca>
2026-02-24 20:18:13 -0800

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private CharacterController cc;
+    [SerializeField] private Animator anim;
+    [SerializeField] private GameObject model;
+    [SerializeField] private Camera cam;
+
+    private float rotateToFaceMovementSpeed = 5f;
+    private float rotateToFaceAwayFromCameraSpeed = 5f;
+
+    private float speed = 9.0f;         // XZ movement speed
+    private float rotationSpeed = 720f; // rotation sensitivity
+
+    private float gravity = -9.81f;     // default gravity (this will change)
+    private float yVelocity = 0f;       // current y Velocity
+    private float yVelocityWhenGrounded = -4f;  // this ensures cc.isGrounded will work 
+
+    private float jumpHeight = 3.0f;    // the height of our jump in units
+    private float jumpTime = 0.5f;      // the time of our jump in seconds
+    private float initialJumpVelocity;  // upward velocity for jumping (precalculated)
+
+    private float jumpsAvailable = 0;
+    private float jumpsMax = 2;
+
+    private void Start()
+    {
+        // calculate gravity & initial jump velocity required for our jump
+        float timeToApex = jumpTime / 2.0f;
+        gravity = (-2 * jumpHeight) / Mathf.Pow(timeToApex, 2);
+        initialJumpVelocity = Mathf.Sqrt(jumpHeight * -2 * gravity);
+    }
+
+    void Update()
+    {
+        // determine XZ movement vector
+        float horizInput = Input.GetAxis("Horizontal");
+        float vertInput = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizInput, 0, vertInput);
+
+        // ensure diagonal movement doesn't exceed horiz/vert movement speed
+        movement = Vector3.ClampMagnitude(movement, 1.0f);
+
+        anim.SetFloat("velocity", movement.magnitude);
+
+        // convert from local to global coordinates
+        movement = transform.TransformDirection(movement);
+
+        if (movement.magnitude > 0)
+        {
+            RotateModelToFaceMovement(movement);
+            RotatePlayerToFaceAwayFromCamera();
+        }
+
+        movement *= speed;
+
+        // calculate yVelocity and add it to the player's movement vector
+        yVelocity += gravity * Time.deltaTime;
+
+        // if we are on the ground and we were falling
+        if (cc.isGrounded && yVelocity < 0.0)
+        {
+            yVelocity = yVelocityWhenGrounded;
+            jumpsAvailable = jumpsMax;
+        }
+
+        // give upward y Velocity if we jumped
+        if (Input.GetButtonDown("Jump") && jumpsAvailable > 0)
+        {
+            yVelocity = initialJumpVelocity;
+            jumpsAvailable--;
+            anim.SetTrigger("jump");
+        }
+
+        anim.SetBool("isGrounded", cc.isGrounded);
+        movement.y = yVelocity;
+
+        movement *= Time.deltaTime; // make all movement processor independent
+
+        // move the player  (using the character controller)
+        cc.Move(movement);
+
+        // rotate the player
+        //Vector3 rotation = Vector3.up * rotationSpeed * Time.deltaTime * Input.GetAxis("Mouse X");
+       //transform.Rotate(rotation);
+
+
+    }
+
+
+    // Set the rotation of the model to match the direction of the movement vector
+    private void RotateModelToFaceMovement(Vector3 moveDirection)
+    {
+        Quaternion newRotation = Quaternion.LookRotation(moveDirection);
+        model.transform.rotation = Quaternion.Slerp(model.transform.rotation, newRotation, rotateToFaceMovementSpeed * Time.deltaTime);
+    }
+
+    // set the player's Y rotation (yaw) to be aligned with the camera's Y rotation
+    private void RotatePlayerToFaceAwayFromCamera()
+    {
+        Quaternion camRotation = Quaternion.Euler(0, cam.transform.rotation.eulerAngles.y, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, camRotation, rotateToFaceAwayFromCameraSpeed * Time.deltaTime);
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        // make the source 1.5 units up from the player's pivot point (at their chest instead of their feet)
+        Vector3 source = transform.position + Vector3.up * 1.5f;
+
+        // visualize what direction the model is facing (blue line)
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(source, source + (model.transform.forward * 3f));
+
+        // visualize what direction the Player is facing (red line)
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(source, source + (transform.forward * 3f));
+    }
+}
